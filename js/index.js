853207d714bc4d257d5a02341d2130f911858f35
alvaromago <alvaromago>
2023-03-21 09:28:24 +0100

    Teoría JavaScript

--- js/index.js
+++ js/index.js
@@ -12,27 +12,52 @@ z=7;
 const b=2;
 x=y*z;
 
+document.write("<p>Primer texto escrito en JavaScript</p>")
 document.write("<p>El resultado de y por z es: " + x + "</p>");
 
-suma(); // El orden de ejecución puede importar dependiendo las variables que se declaren en las funciones
+suma(y, z); // El orden de ejecución puede importar dependiendo las variables que se declaren en las funciones
 resta();
 
-function suma(){
-    $suma = y + z;
+$var1 = 4;
+$var2 = "7";
+document.write("<p>La variable $var1 es de tipo: " + typeof($var1) + "</p>") // type = number
+document.write("<p>La variable $var2 es de tipo: " + typeof($var2) + "</p>") // type = string
+document.write($var1 + ($var2) + "<br>"); // 47
+document.write($var1 + parseInt($var2)); // 11
+
+$var3 = 8;
+$var4 = 0;
+document.write("<p>" + $var3 / $var4 + "</p>"); // Infinity
+
+$var5 = suma($var1, parseInt($var2));
+$var6 = resta($var4, $var1);
+document.write("<table border='1'><tr><td>Suma</td><td>Resta</td></tr>");
+document.write("<tr><td align='center'>" + $var5 + "</td><td align='center'>" + $var6 + "</td></tr></table>");
+
+if(multiplo3(8)){ // Si devuelve true
+    document.write("<p>El número es múltiplo de tres</p>")
+}
+else{
+    document.write("<p>El múltiplo no es número de tres</p>")
+}
+
+function suma($param1, $param2){
+    $suma = $param1 + $param2;
     document.write("<p>La suma de y más z es: " + $suma);
+    resta($param1*2, $param2*3);
+    return $suma;
 }
 
 function resta(){
     $resta = y - z;
     document.write("<p>La resta de y menos z es: " + $resta);
+    return $resta;
 }
 
-$var1 = 4;
-$var2 = "Hola";
-document.write("<p>La variable $var1 es de tipo: " + typeof($var1) + "</p>") // type = number
-document.write("<p>La variable $var2 es de tipo: " + typeof($var2) + "</p>") // type = string
-document.write($var1 + $var2); // 4Hola
-
-$var3 = 8;
-$var4 = 0;
-document.write("<p>" + $var3 / $var4 + "</p>"); // Infinity
+function multiplo3($param1){
+    $esMultiplo = false;
+    if($param1%3 == 0){
+        $esMultiplo = true;
+    }
+    return $esMultiplo;
+}
