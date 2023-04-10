a26040f42f09b94a9f40ca60028f169dce5216cc
Álvaro Martín <aalvaromago@gmail.com>
2023-04-10 21:02:15 +0200

    Update ejercicios 21 & 22

--- js/ejercicio21.js
+++ js/ejercicio21.js
@@ -1,5 +1,5 @@
 document.getElementById("boton1").onclick = function (){
     let nombre = document.getElementById("nombre").value;
-    let mensaje = "Hola", nombre;
+    let mensaje = "Hola " + nombre;
     alert(mensaje);
 }
