82a857d68942db92e76b14e0c703127fe7eadc11
alvaromago <aalvaromago@gmail.com>
2023-04-18 09:31:14 +0200

    Correción ejercicios

--- js/ejercicio10.js
+++ js/ejercicio10.js
@@ -1,8 +1,6 @@
+document.write("<h3>Ejercicio 10</h3>")
 let numeros = [];
 for(i = 0; i < 5; i++){
-    numeros[i] = Math.round(Math.random() * (10 - 1) + (1));
-}
-document.write("<h3>Ejercicio 10</h3>")
-for(i = 0; i < numeros.length; i++){
+    numeros[i] = Math.ceil(Math.random() * 10);
     document.write(numeros[i] + "<br>");
 }
--- js/ejercicio11.js
+++ js/ejercicio11.js
@@ -1,12 +1,10 @@
+document.write("<h3>Ejercicio 11</h3>")
 let numeros1 = [];
 let numeros2 = [];
 let sumaNumeros = [];
 for(i = 0; i < 10; i++){
-    numeros1[i] = Math.round(Math.random() * (10 - 1) + (1));
-    numeros2[i] = Math.round(Math.random() * (10 - 1) + (1));
-}
-document.write("<h3>Ejercicio 11</h3>")
-for(i = 0; i < 10; i++){
+    numeros1[i] = Math.round(Math.random() * 9 + 1);
+    numeros2[i] = Math.round(Math.random() * 9 + 1);
     sumaNumeros[i] = numeros1[i] + numeros2[i];
     document.write(sumaNumeros[i] + "<br>");
 }
